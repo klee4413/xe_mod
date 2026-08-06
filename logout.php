34f7ced5d1024e2a4e97aae75a2ff0599e763c80
Kevin <klee4413@users.noreply.github.com>
2026-08-06 08:40:53 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,9 +4,9 @@ session_start(); // 1. Unset all session variables
 $_SESSION = array();
 // 2. Destroy the session cookie in the browser
 if (ini_get("session.use_cookies")) { $params = session_get_cookie_params();
-                                                         setcookie(session_name(), '', time() - 42000,
-                                                         $params["path"], $params["domain"],
-                                                         $params["secure"], $params["httponly"]);}
+                                      setcookie(session_name(), '', time() - 42000,
+                                      $params["path"], $params["domain"],
+                                      $params["secure"], $params["httponly"]);}
 // 3. Destroy the server-side session
 session_destroy();
 // 4. Return to the Login Gate
