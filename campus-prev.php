c502344c7e85c1e78ef886660b56aad296e8f6cf
Kevin <klee4413@users.noreply.github.com>
2026-08-06 04:20:25 -0700

    Update campus-prev.php

--- campus-prev.php
+++ campus-prev.php
@@ -2,7 +2,7 @@
 // 1. Database Connection campus.php
 session_start();
 require_once __DIR__ . '/../db-connect.php';
-//require_once 'db-config.php';
+ 
 $student_id = $_SESSION['user_id']    ?? 0;
 $first_name = $_SESSION['first_name'] ?? 'Scholar';
 $last_name  = $_SESSION['last_name']  ?? '';
